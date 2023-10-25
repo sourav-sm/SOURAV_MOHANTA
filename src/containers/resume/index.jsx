2eebe02f70c4dcb95de1bfdbd02a4414757978e7
Sourav <souravmohanta4428@gmail.com>
2023-10-25 19:50:00 +0530

    updataing of resume section

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -4,75 +4,96 @@ import PageHeaderContetent from "../../compnents/pageHeaderContent";
 import {VerticalTimeline,VerticalTimelineElement} from "react-vertical-timeline-component";
 import 'react-vertical-timeline-component/style.min.css';
 import {data} from "./utils";
+import './styles.scss';
+import {MdWork} from 'react-icons/md';
 
 const Resume=()=>{
-    return(
-        <section id="resume" className="resume">
-           <PageHeaderContetent
-           headerText="My Resume"
-           icon={<BsInfoCircleFill size={40}/>}
-           />
+  return (
+    <section id="resume" className="resume">
+      <PageHeaderContetent
+        headerText="My Resume"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="timeline">
+        <div className="timeline__experience">
+          <h3 className="timeline__experience__header-text">Experience</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {data.experience.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__vertical-timeline-element"
+                contentStyle={{
+                  background: 'none',
+                  color : 'var(--yellow-theme-sub-text-color)',
+                  border : '1.5px solid var(--yellow-theme-main-color)'
+                }}
+                date="2020 - Present"
+                icon={<MdWork/>}
+                iconStyle={{
+                  background : '#181818',
+                  color : 'var(--yellow-theme-main-color)',
 
-           <div className="timeline">
-              <div className="timeline_experience">
-                <h3 className="timeline_experience_header-text">
-                  Experience
-                </h3>
-                <VerticalTimeline
-                layout={'1-column'}
-                lineColor="var(--yellow-theme-main-color)">
-                  {
-                    data.experience.map((item,i)=>{
-                      <VerticalTimelineElement
-                      key={i}
-                      className="timeline_experience_vertical-timeline-element">
-                        <div className="vertical-timeline-element-title-wrapper">
-                          <h3 className="vertical-timeline-element-title">
-                            {item.title}
-                          </h3>
+                }}
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3>
+                    {item.title}
+                  </h3>
 
-                          <h4 className="vertical-timeline-element-subtitle">
-                            {item.subTitle}
-                          </h4>
-                          <p>{item.description}</p>
-                        </div>
-                      </VerticalTimelineElement>
-                    })
-                  }
-                </VerticalTimeline>
-              </div>
-              <div className="timeline_education">
-              <div className="timeline_experience">
-                <h3 className="timeline_experience_header-text">
-                  Experience
-                </h3>
-                <VerticalTimeline
-                layout={'1-column'}
-                lineColor="var(--yellow-theme-main-color)">
-                  {
-                    data.education.map((item,i)=>{
-                      <VerticalTimelineElement
-                      key={i}
-                      className="timeline_education_vertical-timeline-element">
-                        <div className="vertical-timeline-element-title-wrapper">
-                          <h3 className="vertical-timeline-element-title">
-                            {item.title}
-                          </h3>
+                  <h4>
+                    {item.subTitle}
+                  </h4>
+                </div>
+                <p className="vertical-timeline-element-title-wrapper-description">{item.description} </p>
 
-                          <h4 className="vertical-timeline-element-subtitle">
-                            {item.subTitle}
-                          </h4>
-                          <p>{item.description}</p>
-                        </div>
-                      </VerticalTimelineElement>
-                    })
-                  }
-                </VerticalTimeline>
-              </div>
-              </div>
-           </div>
-        </section>
-      )
-}
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+        <div className="timeline__education">
+          <h3 className="timeline__education__header-text">Education</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+          {data.education.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__vertical-timeline-element"
+                contentStyle={{
+                  background: 'none',
+                  color : 'var(--yellow-theme-sub-text-color)',
+                  border : '1.5px solid var(--yellow-theme-main-color)'
+                }}
+                date="2020 - Present"
+                icon={<MdWork/>}
+                iconStyle={{
+                  background : '#181818',
+                  color : 'var(--yellow-theme-main-color)',
+
+                }}
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3>
+                    {item.title}
+                  </h3>
+
+                  <h4>
+                    {item.subTitle}
+                  </h4>
+                </div>
+                <p className="vertical-timeline-element-title-wrapper-description">{item.description} </p>
+
+              </VerticalTimelineElement>
+            ))}
+            </VerticalTimeline>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Resume;
